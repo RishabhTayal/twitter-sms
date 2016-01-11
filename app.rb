538ca4ec42e8c25fd97d5101b84a3edb29ffffcb
Rishabh Tayal <rtayal11@gmail.com>
2016-01-11 16:07:50 -0600

    commit

--- app.rb
+++ app.rb
@@ -9,8 +9,8 @@ post "/messages" do
   puts "webhook called" 
   p params
   p request.body.read
-  @body = JSON.parse(request.body.read)
-  p @body
+  body = JSON.parse(request.body.read || '{"name":"Not Given"}')
+  p body
   # p request.body.read[:message_data]["bodies"][0]["content"]
   # twitter.update(params["Body"]) #if params["From"] == ENV["MY_PHONE_NUMBER"]
   # content_type "text/xml"
